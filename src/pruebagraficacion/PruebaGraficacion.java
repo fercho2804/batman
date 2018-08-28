010098402573d0d3619260409f680be4bec0494d
Luis Fernando <flecha_punk@hotmail.com>
2018-08-28 17:38:40 -0500

    que tranza

--- src/pruebagraficacion/PruebaGraficacion.java
+++ src/pruebagraficacion/PruebaGraficacion.java
@@ -16,7 +16,7 @@ public class PruebaGraficacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Que tranza carnal");
     }
     
 }
